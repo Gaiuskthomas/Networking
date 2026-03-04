6a4950b5f8cb3b61251e147bc640f83dd0d4a026
Gaius K Thomas <gaiuskandothazhuthu@gmail.com>
2026-03-04 11:22:27 +0530

    server side code

---
+++ exp10/exp10s.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#define PORT 8088
+#define MAXLINE 1024
+// Driver code
+int main() {
+int sockfd;
+char buffer[MAXLINE];
+struct sockaddr_in
+servaddr;
+// Creating socket file descriptor
+if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+perror("socket creation failed");
+exit(EXIT_FAILURE);
+}
+memset(&servaddr, 0, sizeof(servaddr));
+// Filling server information
+servaddr.sin_family = AF_INET;
+servaddr.sin_port = htons(PORT);
+servaddr.sin_addr.s_addr = INADDR_ANY;
+int n, len;
+strcpy(buffer,"TIME");
+sendto(sockfd, buffer, strlen(buffer),
+MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+sizeof(servaddr));
+printf("Time Request sent.\n");
+n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+MSG_WAITALL, (struct sockaddr *) &servaddr,
+&len);
+buffer[n] = '\0';
+printf("Time from Server : %s\n", buffer);
+close(sockfd);
+return 0;
+}
